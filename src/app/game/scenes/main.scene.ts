6d820458accd4571621abd280f6d402b3e96b57b
dastin <astindillon@gmail.com>
2024-02-16 22:40:21 -0500

    added assets and end logic

--- src/app/game/scenes/main.scene.ts
+++ src/app/game/scenes/main.scene.ts
@@ -7,6 +7,10 @@ export class MainScene extends Phaser.Scene {
   paddle: any;
   ball: any;
 
+  bgText!: Phaser.GameObjects.Text;
+  yesBtn!: Phaser.GameObjects.Text;
+  noBtn!: Phaser.GameObjects.Text;
+
   constructor() {
     super({ key: 'MainScene' });
   }
@@ -15,7 +19,7 @@ export class MainScene extends Phaser.Scene {
     brick.disableBody(true, true);
 
     if (this.bricks.countActive() === 0) {
-      this.resetLevel();
+      this.winState();
     }
   }
 
@@ -25,7 +29,25 @@ export class MainScene extends Phaser.Scene {
     this.ball.setData('onPaddle', true);
   }
 
+  winState() {
+    this.resetBall();
+    this.bgText.setText(["You Win!", "Will you be my", "Valentine?"]);
+
+    this.yesBtn = this.add.text(100, 300, "Yes", {color: "#fae7f4"}).setInteractive().on('pointerdown', () => {
+      this.bgText.setText(["YAY!", "Lets Get Weird ;)"]);
+    })
+
+    this.noBtn = this.add.text(300, 300, "No", {color: "#fae7f4"}).setInteractive().on('pointerdown', () => {
+      this.bgText.setText(":(")
+      this.resetLevel();
+    });
+  }
+
   resetLevel() {
+
+    this.noBtn.destroy();
+    this.yesBtn.destroy();
+
     this.resetBall();
 
     this.bricks.children.each((brick: any) => {
@@ -56,22 +78,24 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    console.log('preload method');
+    this.load.atlas('assets', 'assets/spritesheet.png', 'assets/spritesheet.json');
   }
 
   create() {
     this.physics.world.setBoundsCollision(true, true, true, false);
 
+    this.bgText = this.add.text(25, 60, "I Love You Joanna!" ,{font: '32px Courier', color: "#fae7f4" });
+
     this.bricks = this.physics.add.staticGroup({
-      key: 'assets', frame: [ 'blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1' ],
-      frameQuantity: 10,
-      gridAlign: { width: 6, height: 6, cellWidth: 64, cellHeight: 32, x: 20, y: 20 }
+      key: 'assets', frame: [ 'brick1', 'brick2', 'brick3', 'brick4', 'brick5', 'brick6' ],
+      frameQuantity: 6,
+      gridAlign: { width: 6, height: 6, cellWidth: 64, cellHeight: 32, x: 10, y: 10 }
     });
 
-    this.ball = this.physics.add.image(200, 500, 'assets', 'ball1').setCollideWorldBounds(true).setBounce(1);
+    this.ball = this.physics.add.image(200, 500, 'assets', 'head-ball2').setCollideWorldBounds(true).setBounce(1);
     this.ball.setData('onPaddle', true);
 
-    this.paddle = this.physics.add.image(200, 550, 'assets', 'paddle1').setImmovable();
+    this.paddle = this.physics.add.image(200, 550, 'assets', 'j-paddle').setImmovable();
 
     //  Our colliders
     this.physics.add.collider(this.ball, this.bricks, this.hitBrick, undefined, this);
@@ -95,6 +119,9 @@ export class MainScene extends Phaser.Scene {
       }
 
     }, this);
+
+
+
   }
   
   override update(){
